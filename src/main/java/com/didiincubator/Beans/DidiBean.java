fc187efa990c4452ceb077917b97b62b249b3a82
枫叶1 <1>
2016-05-31 22:00:47 +0800

    历史记录，数据获取

--- src/main/java/com/didiincubator/Beans/DidiBean.java
+++ src/main/java/com/didiincubator/Beans/DidiBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * Created by 枫叶1 on 2016/5/24.
+ * 孵化器信息表对象
  */
 public class DidiBean implements Serializable{
     private static final long serialVersionUID = 5192843888413143548L;
@@ -12,8 +13,10 @@ public class DidiBean implements Serializable{
     private String sketch;//简述
     private int detail_id;//详细介绍id
     private int Station_id;//工位情况id
-    private String coordinate;//位置坐标
+    private float coordinateX;
+    private float coordinateY;//位置坐标
     private String type_didi;//孵化器项目类型
+    private String phonenumber;//联系电话
     private String headPortrait;//头像
 
     public int getId() {
@@ -56,14 +59,6 @@ public class DidiBean implements Serializable{
         Station_id = station_id;
     }
 
-    public String getCoordinate() {
-        return coordinate;
-    }
-
-    public void setCoordinate(String coordinate) {
-        this.coordinate = coordinate;
-    }
-
     public String getType_didi() {
         return type_didi;
     }
@@ -80,19 +75,46 @@ public class DidiBean implements Serializable{
         this.headPortrait = headPortrait;
     }
 
+    public float getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(float coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public float getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(float coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
     public DidiBean() {
     }
 
-    public DidiBean(int id, String name, String sketch, int detail_id, int station_id, String coordinate, String type_didi, String headPortrait) {
+    public DidiBean(int id, String name, String sketch, int detail_id, int station_id, float coordinateX, float coordinateY, String type_didi, String phonenumber, String headPortrait) {
         this.id = id;
         this.name = name;
         this.sketch = sketch;
         this.detail_id = detail_id;
         Station_id = station_id;
-        this.coordinate = coordinate;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
         this.type_didi = type_didi;
+        this.phonenumber = phonenumber;
         this.headPortrait = headPortrait;
     }
+
 
     @Override
     public String toString() {
@@ -102,8 +124,10 @@ public class DidiBean implements Serializable{
                 ", sketch='" + sketch + '\'' +
                 ", detail_id=" + detail_id +
                 ", Station_id=" + Station_id +
-                ", coordinate='" + coordinate + '\'' +
+                ", coordinateX=" + coordinateX +
+                ", coordinateY=" + coordinateY +
                 ", type_didi='" + type_didi + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
                 ", headPortrait='" + headPortrait + '\'' +
                 '}';
     }
